dc181b83815c2a6e5566e79555dd6b88e122a734
Saboor Nisha <145483730+SaboorNisha@users.noreply.github.com>
2025-02-04 09:26:24 -0800

    Delete Responsive_landing_page directory

--- Responsive_landing_page/scripts.js
+++
@@ -1,9 +0,0 @@
-// Scroll effect for the navbar
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 100) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
